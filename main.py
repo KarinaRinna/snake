ec5db2cefed077602aaa7ceabb11e86999b79c78
Mandarin <zurual@mail.ru>
2023-07-04 15:48:34 +0300

    добавление счета

--- main.py
+++ main.py
@@ -10,17 +10,23 @@ dirs = {'W': True, 'S': True,'A': True, 'D': True, }
 length = 1
 snake = [(x,y)]
 dx, dy = 0, 0
+score = 0
 fps = 5
 
 pygame.init()
 sc = pygame.display.set_mode([RES, RES])
 clock = pygame.time. Clock()
+font_score = pygame.font.SysFont('Arial', 26, bold=True)
+
 
 while True:
     sc.fill(pygame.Color('black'))
     # рисуем змею
     [(pygame.draw.rect(sc, pygame.Color('green'), (i, j, SIZE, SIZE)))for i, j in snake]
     pygame.draw.rect(sc, pygame.Color('Cyan'),(*apple, SIZE,SIZE))
+    # вывод очков
+    render_score = font_score.render(f'Ваш счет: {score}', 1, pygame.Color('orange'))
+    sc.blit(render_score, (5, 5))
     # передвижение змейки за 1 шаг равно размеру ее головы
     x += dx * SIZE
     y += dy * SIZE
@@ -30,12 +36,11 @@ while True:
     if snake[-1] == apple:  # если яблоко находится в положении головы змеи
         apple = randrange(0, RES, SIZE), randrange(0, RES, SIZE) # создаем новое яблоко
         length += 1 # ширину змейки увеличиваем на 1
+        score += 1  # при сьедании очки +1
         fps += 1    # скорость змейки +1
 
     # игра окончена
-    if x < 0 or x > RES - SIZE or y < 0 or y > RES - SIZE:
-        break
-    if len(snake) != len(set(snake)):
+    if x < 0 or x > RES - SIZE or y < 0 or y > RES - SIZE or len(snake) != len(set(snake)):
         break
 
     pygame.display.flip()
